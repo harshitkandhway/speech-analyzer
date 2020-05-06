3083c71fac100ccb10ccafec2a15ff7af5fb1e37
Harshit Kandhway <kandhway.harshit198@gmail.com>
2020-05-06 12:17:21 +0530

    project initialized

---
+++ src/main/java/com/xebia/speechanalyzer/SpeechAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.xebia.speechanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechAnalyzerApplication.class, args);
+	}
+
+}
