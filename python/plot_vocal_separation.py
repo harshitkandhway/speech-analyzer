cd952f6b90b3c59720dd0a5c19ac416bf7591094
Harshit Kandhway <kandhway.harshit198@gmail.com>
2020-05-11 13:02:46 +0530

    voice separation and voice to text conversion

--- python/plot_vocal_separation.py
+++ python/plot_vocal_separation.py
@@ -9,7 +9,7 @@ import librosa.display
 
 #############################################
 # Load an example with vocals.
-y, sr = librosa.load('audio/Cheese_N_Pot-C_-_16_-_The_Raps_Well_Clean_Album_Version.mp3', duration=120)
+y, sr = librosa.load('speech2.wav', duration=120)
 
 
 # And compute the spectrogram magnitude and phase
