1ef001e2d9b723c35515d5d2f602d1a769e8ee8a
Harshit Kandhway <kandhway.harshit198@gmail.com>
2020-05-14 19:53:11 +0530

    Create speech_demo.py

---
+++ speech_demo.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+
+ 
+
+r = sr.Recognizer()
+# harvard = sr.AudioFile("cutafew.wav")
+# with harvard as source:
+#     audio = r.record(source)
+#     # type(audio)
+# r.recognize_google(audio)
+with sr.Microphone() as source:
+    print("say something")
+    audio = r.listen(source)
+    print("okay")
+try:
+    text = r.recognize_google(audio)
+    print("Text: " + text)
+    f = open("speect.txt", "a+")
+    f.write("\n" + text)
+    f.close
+except:
+    pass
